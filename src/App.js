777f45277ac697dbbc9a3491a216e71d5b45cc6a
Josh A <jmalm79@gmail.com>
2021-05-14 01:29:49 -0500

    updated about me copy

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
       <div>
         <Header />
         <Wrapper>
+          <Route exact path="/react-portfolio/" component={About} />
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/projects" component={Projects} />
