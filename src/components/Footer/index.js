90bccb4b34d46176e64b9c715ed3bbd2be3ecfcd
Josh A <jmalm79@gmail.com>
2021-06-01 14:19:26 -0500

    updatd about

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
     <footer className="footer">
         <ul class="nav nav-pills nav-fill">
           <li class="nav-item"><a class="nav-link" href="mailto:jmalm79@gmail.com">Email: jmalm79@gmail.com</a></li>
-          <li class="nav-item"><div class="nav-link"> Call Me#: 651/325-6365</div></li>
+          <li class="nav-item"><div class="nav-link"> Phone#: 651/325-6365</div></li>
           <li class="nav-item"><a class="nav-link" href="https://www.linkedin.com/in/joshua-alm-011687101" target = "_blank" rel="noopener noreferrer">LinkedIn</a></li>
           <li class="nav-item"><a class="nav-link" href="https://github.com/jmalm79" target = "_blank" rel="noopener noreferrer">GitHub</a></li>
           <li class="nav-item"><a class="nav-link" href={Resume} target = "_blank" rel="noopener noreferrer">Resume</a></li>
--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 import selfie from "../assets/me.png"
+import farmbus from "../assets/farmbus.jpg"
 
 
 console.log(selfie);
@@ -22,13 +23,13 @@ function About() {
           <Col size="sm-12 md-6 lg-7 xl-8">
             <h3>About Me</h3>
                <p>
-               My technical career started in a warehouse around 2008 when I started running a label/print shop for a Medical Device Components Manufacturer. This position required me to step up and self teach new technologies and then train others with. As a casual gamer, I also started tinkering around with modifying my store bought PC, to then building my first (and several after) custom PC. In 2013 when I started as a Level 2 Technician working in a call center. In this role, I helped execute several process enhancements in both the office and warehouse. In the office I designed and implemented a digital troubleshooting guide that replaced the outdated and often incorrect paper copies that the team had been using. In the warehouse, I designed and implemented a color coding structure for our system build process, this helped streamline the and ultimately cut time and reduced costs. In 2017, I moved from the call center to the Web Team as a Website Specialist. I joined the team in time to help finish preparing for the relaunch of their primary E-Commerce site. In late 2019, I received a promotion to Customer Technology Solutions Analyst. In this role, I helped document both internal and external user stories which helped guide the path for the relaunch of a second E-Commerce site. Working with our Developers is what sparked my interest in Web Development and programming.</p>
-               <p>Now, Here I am about to finish the UofM Coding Bootcamp, on my way to becomming a Full Stack Web Developer.         
-              </p>
+               My technical career started around 2008 when I started working in a label/print shop for a Medical Device Components Manufacturer. This position is what motivated me to start exploring more technologies that I was interested in. Being a casual gamer, I started exploring more ways to push and modify my store bought PC. This eventually turned into then building my first (and several after) custom PC. In 2013 I started working as a Level 2 Technician in a call center. In this role, I helped execute several process enhancements in both the office and warehouse. In the office I designed a digital troubleshooting guide that replaced the outdated and often incorrect paper copies that the call center team had been using. In the warehouse, I designed and implemented a color coding structure for our PC system build process, this helped streamline and ultimately reduce build costs. In 2017, I moved from the call center to the Web Team as a Website Specialist. I joined the team in time to help finish preparing for the relaunch of their primary E-Commerce site. In late 2019, I received a promotion to Customer Technology Solutions Analyst. In this role, I helped document both internal and external user stories which helped guide the path for the relaunch of a second E-Commerce site. Working with our Developers is what sparked my interest in Web Development.</p>
+               
             <h3>Hobbies and Other Interests</h3> 
             <p>
-              In my free time, I like to spend time outdoors with my family. Usually going hiking, or going to car shows in our vintage Volkswagen Beetle. 
-            </p> 
+              In my free time, I like to be outdoors with my family, either hiking in a State Parks on the North Shore or out cruising to a car show in our vintage Volkswagen Bus. 
+            </p>
+            <img src={farmbus} height="20%" alt="my VW Bus" />
           </Col>
         </Row>
       </Container>
